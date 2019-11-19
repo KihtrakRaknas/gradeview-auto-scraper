67a124d150ae5b09e3f0506bb5b5244437182796
Kihtrak Raknas <karthikandsankar@gmail.com>
2019-11-18 23:14:49 -0500

    asdf

--- server.js
+++ server.js
@@ -260,7 +260,11 @@ async function scrapeMP(page){
         if(indivClass){
           //indivClass
           await page.evaluate((classID) => changeCourse(classID),indivClass);
-          await page.waitForNavigation({ waitUntil: 'domcontentloaded' })
+          await page.waitForNavigation({ waitUntil: 'domcontentloaded' }).catch(()=>{
+            await browser.close();
+            console.log("Page Failed To Load - indiv class")
+            return {Status:"Page Failed To Load"};
+          })
           const markingPeriods = await page.evaluate( () => (Array.from( (document.getElementById("fldSwitchMP")).childNodes, element => element.value ) ));
           const defaultMP = await page.evaluate(()=>document.getElementById("fldSwitchMP").value);
           markingPeriods.splice(markingPeriods.indexOf(defaultMP), 1);
@@ -302,7 +306,11 @@ async function scrapeMP(page){
                   displayMPs();
                   document.getElementsByTagName("BUTTON")[1].click()//"Switch Marking Period btn"
                 },indivMarkingPeriod);
-                await page.waitForNavigation({ waitUntil: 'domcontentloaded' })
+                await page.waitForNavigation({ waitUntil: 'domcontentloaded' }).catch(()=>{
+                  await browser.close();
+                  console.log("Page Failed To Load - switch MP")
+                  return {Status:"Page Failed To Load"};
+                })
                 
                 //console.log(page.evaluate(()=>{return document.getElementsByClassName("list")[0].getElementsByTagName("span")[0].innerText.match(new RegExp('[0-1]?[0-9]/[0-3]?[0-9]/[0-9][0-9]'))?new Date()-new Date(document.getElementsByClassName("list")[0].getElementsByTagName("span")[0].innerText.match(new RegExp('[0-1]?[0-9]/[0-3]?[0-9]/[0-9][0-9]'))).getTime()>0:false}))
                 if(await page.evaluate(()=>{return document.getElementsByClassName("list")[0].getElementsByTagName("span")[0].innerText.match(new RegExp('[0-1]?[0-9]/[0-3]?[0-9]/[0-9][0-9]'))?new Date()-new Date(document.getElementsByClassName("list")[0].getElementsByTagName("span")[0].innerText.match(new RegExp('[0-1]?[0-9]/[0-3]?[0-9]/[0-9][0-9]'))).getTime()>0:false})){
