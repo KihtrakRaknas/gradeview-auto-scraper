b23c2bbacf1ff6bf76bbc9f90d23ae02d1a1c56a
KihtrakRaknas <karthikandsankar@gmail.com>
2020-07-19 17:45:46 -0400

    Revert "updated the code for reababilty and made it easier to maintain"
    This reverts commit ee7dc7fae75e57ad11c04a1efdcafd2c2891a681.

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
-require('dotenv').config();
-const { getCurrentGrades } = require('./GradeViewGetCurrentGrades/getCurrentGrades');
+const puppeteer = require('puppeteer');
+const $ = require('cheerio');
+const url = 'https://students.sbschools.org/genesis/parents?gohome=true';
 const express = require('express')
 const NodeRSA = require('node-rsa');
-//const keysObj = require('./secureContent/keys')
-const fs = require('fs');
 const key = new NodeRSA({b: 512});
-key.importKey(process.env.PUBLIC_KEY/*keysObj.public*/, 'pkcs1-public-pem');
-key.importKey(process.env.PRIVATE_KEY/*keysObj.private*/, 'pkcs1-private-pem');
+const keysObj = require('./secureContent/keys')
+const fs = require('fs');
+
+key.importKey(keysObj.public, 'pkcs1-public-pem');
+key.importKey(keysObj.private, 'pkcs1-private-pem');
 
 const admin = require('firebase-admin');
 
-var serviceAccount = JSON.parse(process.env.SERVICE_KEY)//require('./secureContent/serviceKey.json');
+var serviceAccount = require('./secureContent/serviceKey.json');
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -83,7 +85,7 @@ function run(){
     }).then(async (users)=>{
       console.log(users.length)
       for(user of users){
-        const maxParalellChromes = 3; // 2 - 20 ; 3 - 20;4-30; 5 -crash
+        const maxParalellChromes = 30; // 2 - 20 ; 3 - 20;4-30; 5 -crash
         if(userDataList.length > maxParalellChromes-1){
           if(userDataList.length!=users.length)
             listObj = userDataList[userDataList.length-maxParalellChromes]
@@ -111,7 +113,7 @@ function run(){
           var userRef = db.collection('users').doc(username);
           console.log("Starting scrape - "+username)
           //if(username == "10015309@sbstudents.org"||username == "10015311@sbstudents.org"){//if(username == "10013096@sbstudents.org"||username == "10012734@sbstudents.org"){
-              var dataObj = getCurrentGrades(username,password)
+              var dataObj = getData(username,password)
               userDataList.push({data:dataObj,username,userRef})
               //console.log(dataObj)
               
@@ -123,6 +125,235 @@ function run(){
     });
   }
 
+
+
+async function scrapeMP(page){
+    var list = await page.evaluate(() => {
+      var assignments = [];
+      for(var node of document.getElementsByClassName("list")[0].childNodes[1].childNodes){
+  
+        if(node.classList && !node.classList.contains("listheading")&&node.childNodes.length>=11){
+          var assignData={};
+  
+          //console.log(node.childNodes);
+          //console.log(node.childNodes[3].innerText);
+            assignData["Date"] = node.childNodes[3].innerText;
+          //console.log(node.childNodes[7].innerText);
+          assignData["Category"] = node.childNodes[7].innerText
+          //console.log(node.childNodes[9].innerText);
+          assignData["Name"] = node.childNodes[9].innerText;
+          //console.log(node.childNodes[11].childNodes[0].textContent.replace(/\s/g,''));
+          if(node.childNodes[11].childNodes.length<=3){
+            assignData["Grade"] = node.childNodes[11].childNodes[0].textContent.replace(/\s/g,'')
+          }else{
+            assignData["Grade"] = node.childNodes[11].childNodes[2].textContent.replace(/\s/g,'')
+            assignData["Weighting"] = node.childNodes[11].childNodes[1].textContent.replace(/\s/g,'')
+          }
+          var commentText = node.childNodes[9].childNodes[3].innerText
+          commentText = commentText.substring(commentText.indexOf("Close")+5).trim()
+          if(commentText!="")
+            assignData["Comment"] = commentText;
+          assignments.push(assignData);
+          }
+      }
+      return assignments;
+    });
+    return list;
+  }
+
+  async function getData(email, pass) {
+      var grades = {};
+    
+        var email = encodeURIComponent(email);
+        pass = encodeURIComponent(pass);
+      var url2 = 'https://students.sbschools.org/genesis/j_security_check?j_username='+email+'&j_password='+pass;
+    
+        const browser = await puppeteer.launch({
+          args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--disable-gpu',
+            '--window-size=1920x1080',
+          ],
+          
+            //headless: false, // launch headful mode
+            //slowMo: 1000, // slow down puppeteer script so that it's easier to follow visually
+          
+          }).catch((err)=>{
+            console.log(err)
+          });
+          if(browser == null){
+            console.log("Chrome Crashed----------------------------------------------------------")
+            return {Status:"Chrome Crashed"};
+          }
+        const page = await browser.newPage();
+        await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3738.0 Safari/537.36');
+    
+        await page.setRequestInterception(true);
+        const blockedResourceTypes = [
+          'image',
+          'media',
+          'font',
+          'texttrack',
+          'object',
+          'beacon',
+          'csp_report',
+          'imageset',
+          'stylesheet',
+        ];
+    
+        const skippedResources = [
+          'quantserve',
+          'adzerk',
+          'doubleclick',
+          'adition',
+          'exelator',
+          'sharethrough',
+          'cdn.api.twitter',
+          'google-analytics',
+          'googletagmanager',
+          'google',
+          'fontawesome',
+          'facebook',
+          'analytics',
+          'optimizely',
+          'clicktale',
+          'mixpanel',
+          'zedo',
+          'clicksor',
+          'tiqcdn',
+        ];
+        page.on('request', (req) => {
+          const requestUrl = req._url.split('?')[0].split('#')[0];
+          if (
+            blockedResourceTypes.indexOf(req.resourceType()) !== -1 ||
+            skippedResources.some(resource => requestUrl.indexOf(resource) !== -1)
+          ) {
+            req.abort();
+          } else {
+            req.continue();
+        }
+        });
+    
+        await page.goto(url, {waitUntil: 'domcontentloaded'});
+        await page.goto(url2, {waitUntil: 'domcontentloaded'});
+    
+        var signedIn = false;
+        if(await $('.sectionTitle', await page.content()).text().trim() != "Invalid user name or password.  Please try again.")
+          signedIn = true;
+        if(!signedIn){
+          await browser.close();
+          console.log("BAD user||pass")
+          return {Status:"Invalid"};
+        }
+    
+        const url3 = "https://students.sbschools.org/genesis/parents?tab1=studentdata&tab2=gradebook&tab3=coursesummary&action=form&studentid="+email.split("%40")[0];
+        await page.goto(url3, {waitUntil: 'domcontentloaded'});
+        
+    
+        //await page.evaluate(text => [...document.querySelectorAll('*')].find(e => e.textContent.trim() === text).click(), "Gradebook");
+        //await page.waitForNavigation({ waitUntil: 'domcontentloaded' })
+      //await page.evaluate(text => [...document.querySelectorAll('*')].find(e => e.textContent.trim() === text).click(), "Course Summary");
+      //await page.waitForNavigation({ waitUntil: 'domcontentloaded' })
+      let classes;
+        try{
+          classes = await page.evaluate( () => (Array.from( (document.getElementById("fldCourse")).childNodes, element => element.value ) ));
+        }catch(err){
+          await browser.close();
+          console.log("No AUP??? - No Courses Found")
+          return {Status:"No Courses Found"};
+        }
+        
+    
+      for(var indivClass of classes){
+        if(indivClass){
+          //indivClass
+          await page.evaluate((classID) => changeCourse(classID),indivClass);
+          let navResult = true;
+          await page.waitForNavigation({ waitUntil: 'domcontentloaded'}).catch((err)=>{
+            console.log(err)
+            console.log("Page Timed-out (switch MP) ----------------------------------------------------------")
+            navResult = false;
+          });
+          if(!navResult){
+            await browser.close();
+            console.log("Page Timed-out received - broswer closed")
+            return {Status: "Page Timed-out"}
+          }
+          const markingPeriods = await page.evaluate( () => (Array.from( (document.getElementById("fldSwitchMP")).childNodes, element => element.value ) ));
+          const defaultMP = await page.evaluate(()=>document.getElementById("fldSwitchMP").value);
+          markingPeriods.splice(markingPeriods.indexOf(defaultMP), 1);
+    
+          const ClassName = await page.evaluate((classID)=>document.querySelectorAll('[value="'+classID+'"]')[0].innerText,indivClass);
+          if(!grades[ClassName])
+            grades[ClassName] = {}
+            
+              grades[ClassName]["teacher"] = await page.evaluate(()=>{
+                  let list = document.getElementsByClassName("list")[0].childNodes[1].childNodes[4].childNodes[5];
+                  if(list)
+                      return list.innerText
+                    else
+                      return null;
+              });
+          if(await page.evaluate(()=>{return document.getElementsByClassName("list")[0].getElementsByTagName("span")[0].innerText.match(new RegExp('[0-1]?[0-9]/[0-3]?[0-9]/[0-9][0-9]'))?new Date()-new Date(document.getElementsByClassName("list")[0].getElementsByTagName("span")[0].innerText.match(new RegExp('[0-1]?[0-9]/[0-3]?[0-9]/[0-9][0-9]'))).getTime()>0:false})){
+            if(!grades[ClassName][defaultMP])
+              grades[ClassName][defaultMP] = {}
+            grades[ClassName][defaultMP]["Assignments"] = await scrapeMP(page);
+            grades[ClassName][defaultMP]["avg"] = await page.evaluate(()=>document.getElementsByTagName("b")[0].innerText.replace(/\s+/g, '').replace(/[^\d.%]/g,''))
+            //console.log(ClassName)
+          }
+          for(var indivMarkingPeriod of markingPeriods){
+            if(indivMarkingPeriod){
+                
+              if(!grades[ClassName]["teacher"]){
+                  grades[ClassName]["teacher"] = await page.evaluate(()=>{
+                  let list = document.getElementsByClassName("list")[0].childNodes[1].childNodes[4].childNodes[5];
+                  if(list)
+                  return list.innerText
+                  else
+                    return null;
+                });
+              }
+                
+                await page.evaluate((indivMP) => {
+                    
+                  document.getElementById("fldSwitchMP").value = indivMP;
+                  displayMPs();
+                  document.getElementsByTagName("BUTTON")[1].click()//"Switch Marking Period btn"
+                },indivMarkingPeriod);
+                let navResult = true;
+                await page.waitForNavigation({ waitUntil: 'domcontentloaded'}).catch((err)=>{
+                  console.log(err)
+                  console.log("Page Timed-out (switch MP) ----------------------------------------------------------")
+                  navResult = false;
+                });
+                if(!navResult){
+                  await browser.close();
+                  console.log("Page Timed-out received - broswer closed")
+                  return {Status: "Page Timed-out"}
+                }
+                
+                //console.log(page.evaluate(()=>{return document.getElementsByClassName("list")[0].getElementsByTagName("span")[0].innerText.match(new RegExp('[0-1]?[0-9]/[0-3]?[0-9]/[0-9][0-9]'))?new Date()-new Date(document.getElementsByClassName("list")[0].getElementsByTagName("span")[0].innerText.match(new RegExp('[0-1]?[0-9]/[0-3]?[0-9]/[0-9][0-9]'))).getTime()>0:false}))
+                if(await page.evaluate(()=>{return document.getElementsByClassName("list")[0].getElementsByTagName("span")[0].innerText.match(new RegExp('[0-1]?[0-9]/[0-3]?[0-9]/[0-9][0-9]'))?new Date()-new Date(document.getElementsByClassName("list")[0].getElementsByTagName("span")[0].innerText.match(new RegExp('[0-1]?[0-9]/[0-3]?[0-9]/[0-9][0-9]'))).getTime()>0:false})){
+                  if(!grades[ClassName][indivMarkingPeriod])
+                    grades[ClassName][indivMarkingPeriod] = {}
+                  //console.log("Scraping page")
+                  grades[ClassName][indivMarkingPeriod]["Assignments"] = await scrapeMP(page);
+                    //console.log("Getting avg")
+                  grades[ClassName][indivMarkingPeriod]["avg"] = await page.evaluate(()=>document.getElementsByTagName("b")[0].innerText.replace(/\s+/g, '').replace(/[^\d.%]/g,''))
+                    //console.log("Done")
+                }
+            }
+          }
+        }
+      }
+      grades["Status"] = "Completed";
+      await browser.close();
+      return grades;
+  }
+
 /* var cronJob = cron.job("15 6 * * *", function(){ //25 7,9,11,13,16 * * *
   run();
 },null,false,"America/New_York"); 
@@ -130,14 +361,6 @@ function run(){
 cronJob.start();*/
 run();
 
-
-
-
-
-
-
-
-// The following are one time use function. They are only here for convenience. 
 const fetch = require("node-fetch");
 //BACK UP USERS
 /*db.collection('userData').get()
